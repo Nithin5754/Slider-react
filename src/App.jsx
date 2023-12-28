b506cd609112a3ad227a2f0411cf5003b091ee6b
Nithin Joji0756 <nithinjoji0756@gmail.com>
2023-12-28 23:24:54 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+
+import data from './data';
+function App() {
+  return <h2>slider project setup</h2>;
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
